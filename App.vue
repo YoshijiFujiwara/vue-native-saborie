23d52b9f26c67ac7a976d12b9963694208730b1d
yoshiji <yfbiology2@gmail.com>
2019-05-22 11:26:16 +0900

    basic navigation

--- App.vue
+++ App.vue
@@ -1,35 +1,42 @@
 <template>
   <view class="container">
-    <text class="text-color-primary">{{ title }}</text>
-    <Hello />
+    <HomeScreen v-if="activeScreen === 'homeScreen'"
+                :testingData="testingData"
+                :navigate="navigate" />
+    <Screen1 v-if="activeScreen === 'screen1'"
+             :navigate="navigate" />
   </view>
 </template>
 
 <script>
-import Hello from './src/components/Hello';
-import HelloScreen from './src/screens/HomeScreen';
-
-export default {
-  components: {
-    Hello,
-    HelloScreen
-  },
-  data () {
-    return {
-      title: 'サボリー'
+  import HomeScreen from './src/screens/HomeScreen'
+  import Screen1 from './src/screens/Screen1'
+  export default {
+    components: {
+      HomeScreen, Screen1
+    },
+    data () {
+      return {
+        activeScreen: 'homeScreen',
+        testingData: 'Value from App Component'
+      }
+    },
+    methods: {
+      navigate (screen) {
+        this.activeScreen = screen
+      }
     }
   }
-}
 </script>
-npm install --save eslint eslint-plugin-vue@next eslint-config-vue
+
 <style>
-.container {
-  background-color: white;
-  align-items: center;
-  justify-content: center;
-  flex: 1;
-}
-.text-color-primary {
-  color: blue;
-}
+  .container {
+    background-color: white;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+  }
+  .text-color-primary {
+    color: blue;
+  }
 </style>
--- src/screens/HomeScreen.vue
+++ src/screens/HomeScreen.vue
@@ -1,32 +1,34 @@
+
 <template>
-  <view class="container">
+  <view>
     <text class="text-color-primary">{{title}}</text>
+    <text>{{testingData}}</text>
     <Hello />
+    <button title="Go to Screen 1" :on-press="() => navigate('screen1')" />
   </view>
 </template>
 
 <script>
-import Hello from '../components/Hello'
-export default {
-  components: {
-    Hello
-  },
-  data () {
-    return {
-      title: 'Home Screen!'
+  import Hello from '../components/Hello'
+  export default {
+    components: {
+      Hello
+    },
+    // props: ['testingData', 'navigate'],
+    props: {
+      testingData: {
+        type: String,
+        required: false
+      },
+      navigate: {
+        type: Function,
+        required: true
+      }
+    },
+    data () {
+      return {
+        title: 'Home Screen!'
+      }
     }
   }
-}
 </script>
-
-<style>
-  .container {
-    background-color: white;
-    align-items: center;
-    justify-content: center;
-    flex: 1;
-  }
-  .text-color-primary {
-    color: blue;
-  }
-</style>
