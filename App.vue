68804598b289d2855e7fb1f3dfa9849e77c00cbc
YoshijiFujiwara <yfbiology2@gmail.com>
2019-05-23 08:22:42 +0900

    app.vueの中身をsrc/index.vueに移す

--- App.vue
+++ App.vue
@@ -1,42 +1,12 @@
 <template>
-  <view class="container">
-    <AppNavigation />
-  </view>
+  <App />
 </template>
 
 <script>
-import HomeScreen from '@/screens/HomeScreen'
-import Screen1 from '@/screens/Screen1'
-import Screen2 from '@/screens/Screen2'
-import { createStackNavigator, createAppContainer } from 'react-navigation'
-
-const HomeStack = createStackNavigator(
-  {
-    Home: HomeScreen,
-    ScreenOne: Screen1,
-    ScreenTwo: Screen2
-  },
-  {
-    initialRouteName: 'Home'
+  import App from './src'
+  export default {
+    components: {
+      App
+    }
   }
-)
-const AppNavigation = createAppContainer(HomeStack)
-
-export default {
-  components: {
-    HomeScreen,
-    AppNavigation
-  }
-}
 </script>
-
-<style>
-  .container {
-    background-color: white;
-    flex: 1;
-    padding: 40px;
-  }
-  .text-color-primary {
-    color: blue;
-  }
-</style>
