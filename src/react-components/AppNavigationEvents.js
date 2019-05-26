39fd2cac1a122b5ce59ccbd2d1b5b36fd094db02
yoshiji <yfbiology2@gmail.com>
2019-05-26 20:23:08 +0900

    スプレッド演算子は美しい

--- src/react-components/AppNavigationEvents.js
+++ src/react-components/AppNavigationEvents.js
@@ -3,10 +3,8 @@ import { NavigationEvents } from 'react-navigation'
 
 class AppNavigationEvents extends React.Component {
   render () {
-    const { onDidFocus } = this.props
-
     return (
-      <NavigationEvents onDidFocus={onDidFocus} />
+      <NavigationEvents {...this.props} />
     )
   }
 }
